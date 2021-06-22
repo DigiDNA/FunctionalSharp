20525c9aec9414f2d7c567f73163fb04f2edf8bb
macmade <macmade@xs-labs.com>
2021-06-22 10:39:33 +0200

    Added public functions for Array.

---
+++ FunctionalSharp/Array.cs
@@ -0,0 +1,37 @@
+﻿/*******************************************************************************
+ * Copyright (c) 2021, DigiDNA
+ * All rights reserved
+ * 
+ * Unauthorised copying of this copyrighted work, via any medium is strictly
+ * prohibited.
+ * Proprietary and confidential.
+ ******************************************************************************/
+
+using System;
+using System.Collections.Generic;
+
+namespace Functional
+{
+    public static class ArrayExtensions
+    {
+        public static U[] Map< T, U >( this T[] self, Func< T, U > f )
+        {
+            return Functions.Map( self, new List< U >(), f ).ToArray();
+        }
+
+        public static U[] CompactMap< T, U >( this T[] self, Func< T, U > f )
+        {
+            return Functions.CompactMap( self, new List< U >(), f ).ToArray();
+        }
+
+        public static T[] Filter< T >( this T[] self, Func< T, bool > f )
+        {
+            return Functions.Filter( self, new List< T >(), f ).ToArray();
+        }
+
+        public static U Reduce< T, U >( this T[] self, U initialResult, Func< U, T, U > f )
+        {
+            return Functions.Reduce( self, initialResult, f );
+        }
+    }
+}
