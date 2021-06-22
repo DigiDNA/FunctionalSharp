74cdd5920007bc8663644235ce7680eccf2809dd
macmade <macmade@xs-labs.com>
2021-06-22 09:45:46 +0200

    Added generic internal functions. Added public functions for List.

---
+++ FunctionalSharp/Functions.cs
@@ -0,0 +1,118 @@
+﻿/*******************************************************************************
+ * Copyright (c) 2021, DigiDNA
+ * All rights reserved
+ * 
+ * Unauthorised copying of this copyrighted work, via any medium is strictly
+ * prohibited.
+ * Proprietary and confidential.
+ ******************************************************************************/
+
+using System;
+using System.Collections.Generic;
+
+namespace Functional
+{
+    internal static class Functions
+    {
+        internal static R Map< T, U, R >( ICollection< T > source, R destination, Func< T, U > f ) where R: ICollection< U >
+        {
+            if( source == null )
+            {
+                throw new ArgumentNullException( nameof( source ) );
+            }
+
+            if( destination == null )
+            {
+                throw new ArgumentNullException( nameof( destination ) );
+            }
+
+            if( f == null )
+            {
+                throw new ArgumentNullException( nameof( f ) );
+            }
+
+            foreach( T o in source )
+            {
+                destination.Add( f( o ) );
+            }
+
+            return destination;
+        }
+
+        internal static R CompactMap< T, U, R >( ICollection< T > source, R destination, Func< T, U > f ) where R: ICollection< U >
+        {
+            if( source == null )
+            {
+                throw new ArgumentNullException( nameof( source ) );
+            }
+
+            if( destination == null )
+            {
+                throw new ArgumentNullException( nameof( destination ) );
+            }
+
+            if( f == null )
+            {
+                throw new ArgumentNullException( nameof( f ) );
+            }
+
+            foreach( T o in source )
+            {
+                if( f( o ) is U u )
+                {
+                    destination.Add( u );
+                }
+            }
+
+            return destination;
+        }
+        
+        internal static R Filter< T, R >( ICollection< T > source, R destination, Func< T, bool > f ) where R: ICollection< T >
+        {
+            if( source == null )
+            {
+                throw new ArgumentNullException( nameof( source ) );
+            }
+
+            if( destination == null )
+            {
+                throw new ArgumentNullException( nameof( destination ) );
+            }
+
+            if( f == null )
+            {
+                throw new ArgumentNullException( nameof( f ) );
+            }
+
+            foreach( T o in source )
+            {
+                if( f( o ) )
+                {
+                    destination.Add( o );
+                }
+            }
+
+            return destination;
+        }
+
+        internal static U Reduce< T, U >( ICollection< T > source, U initialResult, Func< U, T, U > f )
+        {
+            if( source == null )
+            {
+                throw new ArgumentNullException( nameof( source ) );
+            }
+
+            if( f == null )
+            {
+                throw new ArgumentNullException( nameof( f ) );
+            }
+
+            foreach( T o in source )
+            {
+                initialResult = f( initialResult, o );
+            }
+
+            return initialResult;
+        }
+    }
+}
--- FunctionalSharp/List.cs
+++ FunctionalSharp/List.cs
@@ -13,5 +13,25 @@ using System.Collections.Generic;
 namespace Functional
 {
     public static class ListExtensions
-    {}
+    {
+        public static List< U > Map< T, U >( this List< T > self, Func< T, U > f )
+        {
+            return Functions.Map( self, new List< U >(), f );
+        }
+
+        public static List< U > CompactMap< T, U >( this List< T > self, Func< T, U > f )
+        {
+            return Functions.CompactMap( self, new List< U >(), f );
+        }
+
+        public static List< T > Filter< T >( this List< T > self, Func< T, bool > f )
+        {
+            return Functions.Filter( self, new List< T >(), f );
+        }
+
+        public static U Reduce< T, U >( this List< T > self, U initialResult, Func< U, T, U > f )
+        {
+            return Functions.Reduce( self, initialResult, f );
+        }
+    }
 }
